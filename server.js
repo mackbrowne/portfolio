8eb69c600815db8c203164d6aeed30953218a4fc
Mackenzie Browne <mackbrowne@gmail.com>
2013-11-25 14:39:15 -0500

    added favicon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 // New call to compress content
 app.use(express.compress());
-
+app.use(express.favicon());
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3000);
