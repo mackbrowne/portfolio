6af47121bdd04deb0ed88d10046edbfe27f71306
Mackenzie Browne <mackbrowne@gmail.com>
2013-11-25 16:36:12 -0500

    fixed typo in server.js involving favicon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 // New call to compress content
 app.use(express.compress());
-app.user(express.favicon());
+app.use(express.favicon());
 app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 5000;
